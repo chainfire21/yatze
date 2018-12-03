ec0557f3ee12cea8a2e25d330c18179a950a0168
Federico Paredes <federico.paredes@alertlogic.com>
2018-12-03 10:22:14 -0600

    multiset functionality

--- src/yatzy_app.erl
+++ src/yatzy_app.erl
@@ -24,7 +24,7 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
--type slot() :: 'ones'|'twos'|'threes'|'fours'|'fives'|'sixes'|'chance'|'large_straight'|'small_straight'|'one_pair'|'two_pairs'|'three_of_a_kind'|'four_of_a_kind'|'yatzy'|'full_house'.
+-type slot() :: 'ones'|'twos'|'threes'|'fours'|'fives'|'sixes'|'chance'|'large_straight'|'small_straight'|'one_pair'|'two_pairs'|'three_of_a_kind'|'four_of_a_kind'|'yatzy'|'full_house'|'three_pairs'|'five_of_a_kind'|'castle'|'full_straight'|'tower'.
 
 -type slot_type() :: 'upper' | 'lower'.
 
--- src/yatzy_score.erl
+++ src/yatzy_score.erl
@@ -21,75 +21,121 @@ calc(small_straight,Roll) ->
 calc(large_straight,Roll) ->
     large_straight(lists:sort(Roll));
 calc(yatzy,Roll)->
-    yatzy(Roll);
+    yatzy(to_multiset(Roll,#{}));
 calc(full_house,Roll)->
-    full_house(lists:sort(Roll));
+    full_house(to_multiset(Roll,#{}));
 calc(three_of_a_kind,Roll)->
-    three_of_a_kind(lists:sort(Roll));
+    three_of_a_kind(to_multiset(Roll,#{}));
 calc(four_of_a_kind,Roll)->
-    four_of_a_kind(lists:sort(Roll));
+    four_of_a_kind(to_multiset(Roll,#{}));
 calc(one_pair,Roll)->
-    one_pair(lists:sort(Roll));
+    one_pair(to_multiset(Roll,#{}));
 calc(two_pairs,Roll)->
-    two_pairs(lists:sort(Roll)).
+    two_pairs(to_multiset(Roll,#{}));
+calc(three_pairs,Roll)->
+    three_pairs(to_multiset(Roll,#{}));
+calc(five_of_a_kind,Roll)->
+    five_of_a_kind(to_multiset(Roll,#{}));
+calc(full_straight,Roll)->
+    full_straight(Roll);
+calc(castle,Roll)->
+    castle(to_multiset(Roll,#{}));
+calc(tower,Roll)->
+    tower(to_multiset(Roll,#{})).
+
+
+to_multiset([],X)->
+    lists:sort(fun({_,B},{_,D})-> B>D end,maps:to_list(X));
+to_multiset([H|T],Map)->
+    case maps:is_key(H,Map) of
+        true->
+            M = maps:update_with(H,fun(Num)-> Num+1 end,Map),
+            to_multiset(T,M);
+        false->
+            M = maps:put(H,1,Map),
+            to_multiset(T,M)
+    end.
+
 
 filter_and_sum(Num,Roll)->
     lists:sum(lists:filter(fun(Y) -> Y=:=Num end, Roll)).
+
 
 small_straight([1,2,3,4,5])->
     15;
 small_straight(_)->
     0.
 
+
 large_straight([2,3,4,5,6])->
     20;
 large_straight(_)->
     0.
 
-yatzy([X,X,X,X,X])->
+
+yatzy([{_,_}])->
     50;
 yatzy(_)->
     0.
 
-full_house([X,X,Y,Y,Y]) when X/=Y ->
-    2*X+3*Y;
-full_house([X,X,X,Y,Y]) when X/=Y ->
-    3*X+2*Y;
-full_house(_)->
+
+full_house([{X,Xc},{Y,Yc}|_])when Yc>=2,Xc>2->
+    X*3+Y*2;
+full_house(_) ->
     0.
 
-three_of_a_kind([_,_,X,X,X])->
-    3*X;
-three_of_a_kind([_,X,X,X,_])->
-    3*X;
-three_of_a_kind([X,X,X,_,_])->
-    3*X;
-three_of_a_kind(_)->
+
+three_of_a_kind([{A,B}|_]) when B>=3->
+    A*3;
+three_of_a_kind(_) ->
     0.
 
-four_of_a_kind([X,X,X,X,_])->
-    4*X;
-four_of_a_kind([_,X,X,X,X])->
-    4*X;
-four_of_a_kind(_)->
+
+four_of_a_kind([{A,B}|_]) when B>=4->
+    A*4;
+four_of_a_kind(_) ->
     0.
 
-one_pair([_,_,_,X,X])->
-    2*X;
-one_pair([_,_,X,X,_])->
-    2*X;
-one_pair([_,X,X,_,_])->
-    2*X;
-one_pair([X,X,_,_,_])->
-    2*X;
-one_pair(_)->
+
+one_pair([{X,_},{Y,Cy}|_]) when Cy >= 2 ->
+    max(X,Y)*2;
+one_pair([{X,Cx}|_]) when Cx >=2 ->
+    X*2;
+one_pair(_) ->
     0.
 
-two_pairs([X,X,Y,Y,_]) when X/=Y->
+
+two_pairs([{X,_},{Y,Cy}|_]) when Cy >=2 ->
     2*X+2*Y;
-two_pairs([X,X,_,Y,Y]) when X/=Y->
-    2*X+2*Y;
-two_pairs([_,X,X,Y,Y]) when X/=Y->
-    2*X+2*Y;
-two_pairs(_) ->
+two_pairs(_)->
+    0.
+
+
+three_pairs([{A,Ac},{B,Bc},{C,Cc}|_]) when Ac>=2,Bc>=2,Cc>=2 ->
+    A*2+B*2+C*2;
+three_pairs(_)->
+    0.
+
+
+five_of_a_kind([{X,Xc}|_]) when Xc>=5->
+    X*5;
+five_of_a_kind(_) ->
+    0.
+
+
+full_straight([1,2,3,4,5,6]) ->
+    21;
+full_straight(_)->
+    0.
+
+
+castle([{X,Xc},{Y,Yc}|_]) when Xc>=3, Yc>=3->
+    X*3+Y*3;
+castle(_)->
+    0.
+
+
+tower([{X,Xc},{Y,Yc}|_]) when Xc>=4, Yc>=2->
+    X*4+Y*2;
+tower(_)->
     0.
